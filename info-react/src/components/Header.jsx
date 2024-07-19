d9baa02cb941c74bed6dd5b3d7dfb62bb03c2d7e
AbijuruT <abijuruyves@gmail.com>
2024-07-19 12:49:16 +0200

    Completing react fan fact project

--- info-react/src/components/Header.jsx
+++ info-react/src/components/Header.jsx
@@ -1,7 +1,15 @@
+import logo from '../assets/react.svg';
+import '../css/Header.css';
 const Header = () => {
 	return (
 		<header id='main-header'>
-			<h1>Navbar here</h1>
+			<nav className='main__nav'>
+				<div className='logo'>
+					<img src={logo} alt='logo' className='nav__logo' />
+					<h3 className='nav__title'>ReactFacts</h3>
+				</div>
+				<h4 className='nav__subtitle'>React Course-Project 1</h4>
+			</nav>
 		</header>
 	);
 };
--- info-react/src/components/Maincontent.jsx
+++ info-react/src/components/Maincontent.jsx
@@ -1,7 +1,17 @@
+import '../css/MainContent.css';
 const MainContent = () => {
 	return (
 		<main className='main__content'>
-			<h1>Main content here..</h1>
+			<h1 className='main__title'>Fun facts about React</h1>
+			<ul className='fact__list'>
+				<li className='fact__item'>Was first released in 2013</li>
+				<li className='fact__item'>Was originally created by Jordan walke</li>
+				<li className='fact__item'>Has well over 100K stars on GitHub</li>
+				<li className='fact__item'>Is maintained by facebook</li>
+				<li className='fact__item'>
+					Powers thousands of enterprise apps, including mobile apps
+				</li>
+			</ul>
 		</main>
 	);
 };
